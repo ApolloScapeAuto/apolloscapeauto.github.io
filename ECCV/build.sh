a5b44615ce0f46bba4b606269612133063b63097
sibozhang <sibozhang1@gmail.com>
2022-11-15 18:37:04 -0800

    init:apolloscape:web

---
+++ ECCV/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# fis3 -d ./dist release prod
+# cd ./dist
+# tar zcvf app.tar.gz *
+# cd ..
+# mv ./dist/app.tar.gz .
+# rm -rf ./dist
+# mkdir output
+# cp -rf app.tar.gz output
+
+fis3 -d ./dist release prod
+cd ./dist/orp
+tar zcvf app.tar.gz *
+cd ../../
+mv ./dist/orp/app.tar.gz .
+rm -rf ./dist
+mkdir output
+cp -rf app.tar.gz output
--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+# fis3 -d ./dist release prod
+# cd ./dist
+# tar zcvf app.tar.gz *
+# cd ..
+# mv ./dist/app.tar.gz .
+# rm -rf ./dist
+# mkdir output
+# cp -rf app.tar.gz output
+
+# #!/bin/bash
 fis3 -d ./dist release prod
-cd ./dist
+cd ./dist/orp
 tar zcvf app.tar.gz *
-cd ..
-mv ./dist/app.tar.gz .
+cd ../../
+mv ./dist/orp/app.tar.gz .
 rm -rf ./dist
 mkdir output
 cp -rf app.tar.gz output
-
